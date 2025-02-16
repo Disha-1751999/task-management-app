fd12403bfdbc5bd75f0039ef5e5f2da4af305590
Disha-1751999 <80054827+Disha-1751999@users.noreply.github.com>
2025-02-16 12:36:51 +0600

    new push

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,11 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // server:{
-  //   proxy:{
-  //     '/api/':{
-  //       target:"https://task-management-app-server-henna.vercel.app/"
-  //     }
-  //   }
-  // }
+  server:{
+    proxy:{
+      '/api/':{
+        target:"https://task-management-app-server-henna.vercel.app"
+      }
+    }
+  }
 })
